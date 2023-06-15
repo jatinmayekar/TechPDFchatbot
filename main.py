22f9f96d0c73a2b225bd557a94f2c43781f88d69
Jatin Mayekar <44420409+jatinmayekar@users.noreply.github.com>
2023-06-15 03:19:17 -0400

    basic setup with fixed pdfs

---
+++ main.py
@@ -0,0 +1,44 @@
+from unstructured.partition.auto import partition
+link="/home/jam/Downloads/3HAC050917 TRM RAPID RW 6-en.pdf";
+elements = partition(filename=link,content_type="application/pdf")
+
+from unstructured.documents.elements import NarrativeText
+from unstructured.partition.text_type import sentence_count
+from langchain.text_splitter import NLTKTextSplitter
+
+input=""
+for element in elements:
+    if isinstance(element, NarrativeText):
+        input=input+str(element);
+        
+       
+nltksplit = NLTKTextSplitter(chunk_size=250)
+nsplit = nltksplit.split_text(input) 
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+import os
+os.environ["OPENAI_API_KEY"] = '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'
+
+embed = OpenAIEmbeddings()
+
+db = Chroma.from_texts(nsplit, embed)
+
+query = "how to define a boolean"
+outputtext = db.similarity_search(query)
+print(outputtext[0].page_content)
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+memory=ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+
+from langchain.llms import OpenAI
+qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), db.as_retriever(), memory=memory)
+
+query = "What is this manual about?"
+result = qa({"question": query})
+result["answer"]
+
+query = "tell me how do I move my robot linearly and circularly"
+result = qa({"question": query})
+result["answer"]
